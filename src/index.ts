64830bd064352b46a2c3d3b15445fecf3c53734a
Lucas Gomes <3GOMESz@gmail.com>
2022-04-04 16:44:35 -0300

    basic files

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { Dimensions, useWindowDimensions } from "react-native";
+
+export const vw = (number: number, baseWidth?: number) =>
+  (baseWidth || Dimensions.get("window").width) * (number / 100);
+
+export const vh = (number: number, baseHeight?: number) =>
+  (baseHeight || Dimensions.get("window").height) * (number / 100);
+
+export const vmin = (
+  number?: number,
+  baseWidth?: number,
+  baseHeight?: number
+) =>
+  Math.min(
+    (baseWidth || Dimensions.get("window").width) * (number ? number / 100 : 1),
+    (baseHeight || Dimensions.get("window").height) *
+      (number ? number / 100 : 1)
+  );
+
+export const vmax = (
+  number?: number,
+  baseWidth?: number,
+  baseHeight?: number
+) => {
+  return Math.max(
+    (baseWidth || Dimensions.get("window").width) * (number ? number / 100 : 1),
+    (baseHeight || Dimensions.get("window").height) *
+      (number ? number / 100 : 1)
+  );
+};
+
+export const percentage = (partial: number, total: number) =>
+  (total * partial) / 100;
+
+const useUnits = () => {
+  const { width, height } = useWindowDimensions();
+
+  return {
+    vw: (number: number) => vw(number, width),
+    vh: (number: number) => vh(number, height),
+    vmin: (number?: number) => vmin(number, width, height),
+    vmax: (number?: number) => vmax(number, width, height),
+    percentage: (partial: number, total: number) => percentage(partial, total),
+  };
+};
+
+export default useUnits;
